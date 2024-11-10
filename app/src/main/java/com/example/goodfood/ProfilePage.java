6f2e837032e879921553a622a90c03ae27d8128a
Joan-yy <147325024+Joan-yy@users.noreply.github.com>
2024-11-10 18:23:05 +0800

    Profile-1: home page profileButton navigate to profilePage

---
+++ app/src/main/java/com/example/goodfood/ProfilePage.java
@@ -0,0 +1,42 @@
+package com.example.goodfood;
+
+import static com.example.goodfood.R.id.homeButton;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ProfilePage extends AppCompatActivity {
+
+    private ImageButton cameraButton, profileButton2, homeButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_profile_page);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        homeButton = findViewById(R.id.homeButton);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfilePage.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+}
