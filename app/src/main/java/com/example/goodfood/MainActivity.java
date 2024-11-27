d4406e493c939c31549d0b5d3e9f817fa6d5e5ed
szeyu <szeyusim@gmail.com>
2024-11-27 09:42:30 +0800

    remove all the unused import and the button declaration which has been moved to bottom navigation fragment

--- app/src/main/java/com/example/goodfood/MainActivity.java
+++ app/src/main/java/com/example/goodfood/MainActivity.java
@@ -1,25 +1,13 @@
 package com.example.goodfood;
 import android.content.Intent;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.view.View;
-import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 public class MainActivity extends AppCompatActivity {
-
-    private ImageButton homeButton;
-    private ImageButton cameraButton;
-    private ImageButton profileButton;
-    //private ImageButton profileButton = findViewById(R.id.profileButton);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +16,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
-
 
         public void toTodayStats(View view){
             Intent intent = new Intent(this, DashboardActivity.class);
