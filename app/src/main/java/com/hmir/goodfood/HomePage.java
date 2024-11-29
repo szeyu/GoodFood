0d4748b5d03878ec144cc05d18433412c8ba7082
szeyu <szeyusim@gmail.com>
2024-11-30 00:07:09 +0800

    refractor the main activity to become logical activity class to handle navigation at the start to login page or home page

---
+++ app/src/main/java/com/hmir/goodfood/HomePage.java
@@ -0,0 +1,38 @@
+package com.hmir.goodfood;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_home_page);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void toTodayStats(View view){
+        Intent intent = new Intent(this, DashboardActivity.class);
+        intent.putExtra("code", "today");
+        startActivity(intent);
+    }
+
+    public void toThisMonthStats (View view){
+        Intent intent = new Intent(this, DashboardActivity.class);
+        intent.putExtra("code", "month");
+        startActivity(intent);
+    }
+}
--- app/src/main/java/com/hmir/goodfood/MainActivity.java
+++ app/src/main/java/com/hmir/goodfood/MainActivity.java
@@ -1,11 +1,15 @@
 package com.hmir.goodfood;
-import android.content.Intent;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,19 +19,43 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        // Check the user's authentication status
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (currentUser != null) {
+            // User is signed in
+            // Print all user info
+            printUserInfo(currentUser);
+
+            // redirect to home page
+            redirectToHomePage();
+        } else {
+            // User is not signed in, redirect to login page
+            redirectToLoginPage();
+        }
+
+        // Finish MainActivity as it's only for redirection purposes
+        finish();
     }
 
-        public void toTodayStats(View view){
-            Intent intent = new Intent(this, DashboardActivity.class);
-            intent.putExtra("code", "today");
-            startActivity(intent);
+    private void printUserInfo(FirebaseUser user){
+        String TAG = "Firebase";
+        if (user != null) {
+            // Print user info
+            Log.d(TAG, "User Display Name: " + user.getDisplayName());
+            Log.d(TAG, "User Email: " + user.getEmail());
+            Log.d(TAG, "User Photo URL: " + user.getPhotoUrl());
+            Log.d(TAG, "User UID: " + user.getUid());
         }
+    }
 
-        public void toThisMonthStats (View view){
-            Intent intent = new Intent(this, DashboardActivity.class);
-            intent.putExtra("code", "month");
-            startActivity(intent);
-        }
+    private void redirectToHomePage() {
+        Intent intent = new Intent(MainActivity.this, HomePage.class);
+        startActivity(intent);
+    }
 
-
+    private void redirectToLoginPage() {
+        Intent intent = new Intent(MainActivity.this, LoginPage.class);
+        startActivity(intent);
+    }
 }
