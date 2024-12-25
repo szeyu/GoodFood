1496a35d5370828ce16c698f963f3e521cf5fec0
szeyu <szeyusim@gmail.com>
2024-12-25 23:08:26 +0800

    resolve Non-static nested class found         JAVA-W1019
            Key changes made:
    
            Added static modifier to ResultViewHolder class
            Added @NonNull annotations where appropriate
            Made fields final where possible
            Moved binding logic into the ViewHolder class
            Made ViewHolder fields private and final
            Added proper method to handle binding in ViewHolder

--- app/src/main/java/com/hmir/goodfood/ResultAdapter.java
+++ app/src/main/java/com/hmir/goodfood/ResultAdapter.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
@@ -29,37 +30,48 @@ public class ResultAdapter extends RecyclerView.Adapter<ResultAdapter.ResultView
     }
 
     @Override
-    public void onBindViewHolder(ResultViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ResultViewHolder holder, int position) {
         Item item = items.get(position);
-        holder.itemTitle.setText(item.getName());
-        holder.itemDescription.setText(item.getDescription());
-
-        // Use Glide to load images
-        int imageResourceId = context.getResources().getIdentifier(item.getImageResourceName(), "drawable", context.getPackageName());
-        if (imageResourceId != 0) {
-            Glide.with(context).load(imageResourceId).into(holder.itemImage);
-        } else {
-            holder.itemImage.setImageResource(R.drawable.food_image_placeholder);  // Fallback image
-        }
-
+        holder.bind(item, context);
     }
-
 
     @Override
     public int getItemCount() {
         return items.size();
     }
 
-    class ResultViewHolder extends RecyclerView.ViewHolder {
-        TextView itemTitle;
-        TextView itemDescription;
-        ImageView itemImage;
+    // Made static and added binding logic inside ViewHolder
+    static class ResultViewHolder extends RecyclerView.ViewHolder {
+        private final TextView itemTitle;
+        private final TextView itemDescription;
+        private final ImageView itemImage;
 
-        public ResultViewHolder(View itemView) {
+        public ResultViewHolder(@NonNull View itemView) {
             super(itemView);
             itemTitle = itemView.findViewById(R.id.item_title);
             itemDescription = itemView.findViewById(R.id.item_description);
             itemImage = itemView.findViewById(R.id.item_image);
+        }
+
+        // Added bind method to handle item binding
+        public void bind(Item item, Context context) {
+            itemTitle.setText(item.getName());
+            itemDescription.setText(item.getDescription());
+
+            // Use Glide to load images
+            int imageResourceId = context.getResources().getIdentifier(
+                    item.getImageResourceName(),
+                    "drawable",
+                    context.getPackageName()
+            );
+
+            if (imageResourceId != 0) {
+                Glide.with(context)
+                        .load(imageResourceId)
+                        .into(itemImage);
+            } else {
+                itemImage.setImageResource(R.drawable.food_image_placeholder);
+            }
         }
     }
 }
